fe7b5874ad968a376ab774d2a2a12bb2de0ef36f
superogira <superogira@hotmail.com>
2021-07-05 02:15:42 +0700

    Update condb.php
    กำหนด Timezone

--- condb.php
+++ condb.php
@@ -8,6 +8,7 @@ date_default_timezone_set('Asia/Bangkok');
 
 try {
     $DBH = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
+    $DBH->exec("SET time_zone = '+07:00';");
 }
 catch(PDOException $e)
     {
